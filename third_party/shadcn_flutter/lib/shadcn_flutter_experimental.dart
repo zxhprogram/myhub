fed10940e297c3ccd29e91354a8b8cbd5ac6be1a
zhaoxiuhuan <zhaoxh5@chinatowercom.cn>
2026-08-28 21:42:06 +0800

    chore: 删除第三方依赖 shadcn_flutter 相关的所有文件
    该提交移除了整个 third_party/shadcn_flutter 目录，包括其所有字体资源、代码文件、文档和配置文件，不再使用该第三方包。

--- third_party/shadcn_flutter/lib/shadcn_flutter_experimental.dart
+++
@@ -1,5 +0,0 @@
-library;
-
-export 'shadcn_flutter.dart';
-
-export 'src/previewer/previewer.dart';
--- third_party/shadcn_flutter/lib/shadcn_flutter_extension.dart
+++
@@ -1,5 +0,0 @@
-library;
-
-// This file exports extensions only.
-
-export 'src/theme/theme_extension.dart';
